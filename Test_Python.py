7e119672aca06ade48953df58dd0585e87f0a4d7
Atarios17 <artorian@wp.pl>
2025-03-21 08:23:18 +0100

    Added square root calcuationg to f

--- Test_Python.py
+++ Test_Python.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 
-w = np.arange(0,21,2)
+w = np.arange(0,78,2)
 v = w**2
 
 def f(x,y):
+    x = x**2
+    y = sqrt(y)
     return x+y
 
-z = f(w,v)
+z = f(w,v) + 2137
